4d693c3a8ce1cea62802a098464ae38994642e93
Toreole <toreole.official@gmail.com>
2021-04-24 23:44:55 +0200

    Asset Imports, Main Menu stub, Loading Screen (Why?)

---
+++ LudumDare48/Assets/Scripts/Runtime/Menu/LoadingScreen.cs
@@ -0,0 +1,100 @@
+﻿using TMPro;
+using UnityEngine;
+using DG.Tweening;
+
+namespace LD48
+{
+    ///<summary>The loading screen as an addressable instance class.</summary>
+    public class LoadingScreen : MonoBehaviour
+    {
+        private static LoadingScreen monoInstance;
+
+        [SerializeField]
+        private RectTransform backgroundImage;
+
+        [SerializeField]
+        CanvasGroup itemGroup;
+        [SerializeField]
+        private RectTransform[] loadingBlobs;
+        [SerializeField]
+        float phaseOffset, blobTimeScale, blobMinSize, blobMaxSize;
+        [SerializeField]
+        private TextMeshProUGUI loadingText;
+
+        // Start is called before the first frame update
+        void Awake()
+        {
+            if(monoInstance)
+                Destroy(gameObject);
+            else
+            {
+                monoInstance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+
+        private System.Collections.IEnumerator Start() 
+        {
+            DOTween.Init(true, true, LogBehaviour.ErrorsOnly).SetCapacity(10, 50);
+            this._Show();
+            yield return new WaitForSeconds(3);
+            this._Hide();
+        }
+
+        private void Update()
+        {
+            //Theres gotta be a better way of doing this lmao.
+            string loadTxt;
+            float rTime = Time.time * 3f % 3f;
+            if(rTime <= 1f)
+                loadTxt = "Loading.";
+            else if(rTime <= 2f)
+                loadTxt = "Loading..";
+            else
+                loadTxt = "Loading...";
+            loadingText.text = loadTxt;
+
+            float blobTime = Time.time * blobTimeScale;
+            for(int i = 0; i < loadingBlobs.Length; i++)
+            {
+                float phase = (float)i * phaseOffset * Mathf.Deg2Rad;
+                phase += blobTime;
+                //phase *= Mathf.Deg2Rad;
+                float blobSize = Mathf.Lerp(blobMinSize, blobMaxSize, Mathf.Sin(phase));
+                loadingBlobs[i].localScale = new Vector3(blobSize, blobSize, 1);
+            }
+        }
+
+        public static void Show() => monoInstance._Show();
+
+        private void _Show()
+        {
+            Sequence showSequence = DOTween.Sequence();
+            //Move the background into place.
+            showSequence.Append(backgroundImage.DOAnchorPos(Vector2.zero, duration: 1.0f));
+            //show the other items in the loading screen.
+            showSequence.Append(itemGroup.DOFade(endValue: 1.0f, duration: 0.4f));
+            showSequence.PlayForward();
+        }
+
+        public static void Hide() => monoInstance._Hide();
+        
+        private void _Hide()
+        {
+            Sequence hideSequence = DOTween.Sequence();
+            //hide the other items in the loading screen.
+            hideSequence.Append(itemGroup.DOFade(endValue: 0.0f, duration: 0.4f));
+            //Move the background away from the screen
+            hideSequence.Append(backgroundImage.DOAnchorPos(new Vector2(0, 1500), duration: 1.0f));
+            hideSequence.PlayForward();
+        }
+
+        public static void Create()
+        {
+            if(monoInstance)
+                return;
+            UnityEngine.SceneManagement.SceneManager.LoadScene("LoadingScreen");
+        }
+
+    }
+}
---
+++ LudumDare48/Assets/Scripts/Runtime/Menu/MainMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace LD48
+{
+    ///<summary>This class contains most, if not all of the functionality for the main menu.</summary>
+    public class MainMenu : MonoBehaviour
+    {
+        [SerializeField]
+        private string gameScene = "Gaming";
+
+        [SerializeField]
+        private Button[] uiButtons;
+
+        private void Start() 
+        {
+            LoadingScreen.Create(); 
+        }
+
+        public void PlayGame()
+        {
+            //disable buttons, then load the scene.
+            for(int i = 0; i < uiButtons.Length; i++)
+                uiButtons[i].interactable = false;
+            SceneManager.LoadScene(gameScene);
+        }
+
+        //just a smooth way to get into the game.
+        private IEnumerator LoadGame()
+        {
+            //TODO: fade out the menu music during the transition
+            LoadingScreen.Show();
+            var operation = SceneManager.LoadSceneAsync(gameScene);
+            operation.allowSceneActivation = false;
+            //when the scene cant auto activate, progress is halted at 0.9, so check for that.
+            yield return new WaitUntil(() => operation.progress >= 0.9f);
+            LoadingScreen.Hide();
+            operation.allowSceneActivation = true;
+        }
+
+        //lol just quit.
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
